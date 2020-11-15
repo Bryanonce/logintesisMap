2a722f31c8060266f3def1fa41ef11768ae1d349
Bryan <bradcede@espol.edu.ec>
2020-11-14 19:47:31 -0500

    Tesis login Google

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCGd51IPnB9r6RnQSFdz6ycJ_skGKGY9rY",
+    authDomain: "login-1e7b9.firebaseapp.com",
+    databaseURL: "https://login-1e7b9.firebaseio.com",
+    projectId: "login-1e7b9",
+    storageBucket: "login-1e7b9.appspot.com",
+    messagingSenderId: "1037553732139",
+    appId: "1:1037553732139:web:eb6589d4a7780370cef86e",
+    measurementId: "G-SDJF1J396X"
+};
